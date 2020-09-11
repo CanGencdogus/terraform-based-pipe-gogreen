33f81441b071ed06b3feb44026449411ee28e4c0
cloud-team-1 <cloud_team_1@yahoo.com>
2020-09-11 14:59:08 -0400

    ops

--- gogreenCode/mainGeg.tf
+++ gogreenCode/mainGeg.tf
@@ -43,7 +43,7 @@ resource "aws_kms_key" "replica" {
 }
 
 module "log_bucket" {
-  source = "/d/Cloud-Project/SourceCode1"
+  source = ".//d/Cloud-Project/SourceCode1"
 
   bucket                         = "logs-gogreen"
   acl                            = "log-delivery-write"
@@ -51,7 +51,7 @@ module "log_bucket" {
   attach_elb_log_delivery_policy = true
 }
 module "replica_bucket" {
-  source = "/d/Cloud-Project/SourceCode1"
+  source = ".//d/Cloud-Project/SourceCode1"
 
   providers = {
     aws = aws.replica
@@ -66,7 +66,7 @@ module "replica_bucket" {
 }
 
 module "s3_bucket" {
-  source = "/d/Cloud-Project/SourceCode1"
+  source = ".//d/Cloud-Project/SourceCode1"
 
   bucket = local.bucket_name
   acl    = "private"
