a069770092a9704857a08230a0e9b74d2fef43c7
cloud-team-1 <cloud_team_1@yahoo.com>
2020-09-10 20:26:51 -0400

    removed one backend

--- gogreenCode/backend.tf
+++ gogreenCode/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "gogreen-pipeline-team1"
-    key    = "myec2/ec2-rady-for-terraform-web"
-    region = "us-west-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "gogreen-3tier-tf-state-2"
+#    key    = "myec2/ec2-rady-for-terraform-web"
+#    region = "us-west-1"
+#  }
+#}
